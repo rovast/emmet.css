3f083f1927923108a39ff7c955aba736b86c9a9b
rovast <rovast@163.com>
2020-04-02 17:18:33 +0800

    ♻️ 重新组织代码结构

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,49 @@
 const {series, src, dest} = require('gulp');
-const sass = require('gulp-sass')
-const cssbeautify = require('gulp-cssbeautify');
-const clean = require('gulp-clean');
+const sass = require('gulp-sass');
+const cleanCSS = require('gulp-clean-css');
+const rename = require('gulp-rename');
+const header = require('gulp-header');
+const fs = require('fs');
 
-// 准备目录
-function prepareDirectory() {
-    return src('dist')
-        .pipe(clean());
-}
+const pkg = JSON.parse(fs.readFileSync('package.json'));
+const banner = [
+    '@charset "UTF-8";\n',
+    '/*!',
+    ' * <%= name %> -<%= homepage %>',
+    ' * Version - <%= version %>',
+    ' * Licensed under the MIT license - https://opensource.org/licenses/MIT',
+    ' *',
+    ' * Copyright (c) <%= new Date().getFullYear() %> <%= author.name %>',
+    ' */\n\n',
+].join('\n');
 
 // 编译 scss
-function compileSass() {
-    return src('src/*.scss')
+function build() {
+    return src('emmet.scss')
         .pipe(sass())
-        .pipe(dest('dist'));
+        .pipe(dest('./'));
 }
 
 // 格式化 css
-function formatCss() {
-    return src('dist/*.css')
-        .pipe(cssbeautify())
-        .pipe(dest('dist'));
+function format() {
+    return src('emmet.css')
+        .pipe(cleanCSS({format: 'beautify'}))
+        .pipe(dest('./'));
 }
 
-exports.default = series(prepareDirectory, compileSass, formatCss);
+// 压缩 css
+function minify() {
+    return src('emmet.css')
+        .pipe(cleanCSS())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(dest('./'));
+}
+
+// 增加头部信息
+function addHeader() {
+    return src('*.css')
+        .pipe(header(banner, pkg))
+        .pipe(dest('./'));
+}
+
+exports.default = series(build, format, minify, addHeader);
